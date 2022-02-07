1f8a027f8be294c9a0c9850fdae5664c9d82e709
abdalla665 <abdallatalaat665@gmail.com>
2022-02-07 03:29:40 -0800

    my first commit

---
+++ store/views.py
@@ -0,0 +1,58 @@
+# Django modules
+from django.shortcuts import render, get_object_or_404
+
+# Django locals
+from store.models import Product
+from category.models import Category
+
+# Create your views here.
+
+
+def store(request,category_slug=None):
+
+	# Define categories and products are None
+	categories = None
+	products   = None
+
+	# What if categories slug are NOT None or exist
+	# Return the slugs if they are exist, or
+	# return 404 error if they are not exist
+	if category_slug != None:
+		categories = get_object_or_404(Category, slug=category_slug)
+		products   = Product.objects.filter(category=categories, is_available=True)
+
+		# Product count
+		product_count = products.count()
+
+	else:
+
+		# Get all the available products
+		products = Product.objects.all().filter(is_available=True)
+
+		# Counting the products
+		product_count = products.count()
+
+	# Put the available products into context dictionary
+	context = {
+		'products':products, # <-- 'products'  as variable
+		'product_count':product_count,
+	}	
+
+	return render(request, 'store/store.html', context)
+
+def product_detail(request,category_slug,product_slug):
+	# Get the slug from Category model and slug from the Product model
+	try: 
+		single_product = Product.objects.get(category__slug=category_slug,slug=product_slug)
+		products = Product.objects.all().filter(is_available=True)
+		
+	except Exception as e: 
+		raise e
+
+	# Put the available products into context dictionary
+	context = {
+		'single_product':single_product, # <-- 'single_product'  as variable
+		'products': products,
+	}
+	return render(request, 'store/product_detail.html', context)
+
